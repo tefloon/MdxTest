7f37e9a28e1d5046f8b8bfd46fb3d6a2bdef835e
tefloon <tefloon@gmail.com>
2023-11-14 13:53:55 +0100

    working mdx, no styling

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import MyMdxText from "@/mdx/test.mdx";
+import React from "react";
+
+export default function Test() {
+  return (
+    <>
+      <h1>Test</h1>
+      <MyMdxText />
+    </>
+  );
+}
